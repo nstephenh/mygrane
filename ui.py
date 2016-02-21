fc8300684e8e5a29e51103f6ffbadca2669c3d03
Noah Stephen Haskell <nstephenh@gmail.com>
2016-02-20 19:40:17 -0500

    Added test covers and added cover images to UI

--- ui.py
+++ ui.py
@@ -1,8 +1,11 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
-stuff = ["A comic","ANother comic","Number 4","Stuff","Ryan","Yet another thing"]
+stuff = [["Test 1", "./Example Covers/test cover1.jpg"],
+         ["Test 2", "./Example Covers/test cover 2.jpg"],
+         ["Test 3", "./Example Covers/test cover 3.jpg"],
+         ["Test 4", "./Example Covers/test cover 4.jpg"]]
 
 titleFlow = Gtk.FlowBox()
 titleFlow.set_valign(Gtk.Align.START)
@@ -16,22 +19,22 @@ class LibraryManager(Gtk.Window):
         titleScroll.add(titleFlow)
 
         self.set_default_size(600, 700)
-        #self.populateTitles()
         self.add(titleScroll)
         self.show_all()
 
-    def addTitle(self, titleObject):
+    def add_title(self, title_object):
         button = Gtk.Button()
-        button.set_label(titleObject)
-        print('added a button: ' + titleObject)
+        #button.set_label(title_object[0])
+        button.add(Gtk.Image.new_from_file(title_object[1]))
+        print('added a cover: ' + title_object[0])
         return button
-    def populateTitles(self):
+
+    def populate_titles(self):
         for title in stuff:
-            titleFlow.add(self.addTitle(title))
+            titleFlow.add(self.add_title(title))
         self.show_all()
 
-
 mainWindow = LibraryManager()
-mainWindow.populateTitles()
+mainWindow.populate_titles()
 mainWindow.connect("delete-event", Gtk.main_quit)
 Gtk.main()
