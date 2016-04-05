2e96956deb96143ac62638a7440edf19658495a8
Noah Stephen Haskell <nstephenh@gmail.com>
2016-04-05 10:38:13 -0400

    Redid spacing in CMC-Rename

--- Scripts/CMC-Rename.py
+++ Scripts/CMC-Rename.py
@@ -1,11 +1,12 @@
 import os
+
 walk = os.walk(".")
 for directory in walk:
-	for filename in directory[2]:
-		print filename
-		extension = filename.split(".")[-1]
-		if extension == "cbz" or extension == "cbr":
-			year = filename[:4]
-			newname =  filename[7:-4] + " (" + year + ")" + "." + extension
-			print newname
-			os.system("mv '" + filename + "' '" + newname + "'")
+    for filename in directory[2]:
+        print(filename)
+        extension = filename.split(".")[-1]
+        if extension == "cbz" or extension == "cbr":
+            year = filename[:4]
+            newname = filename[7:-4] + " (" + year + ")" + "." + extension
+            print(newname)
+            os.system("mv '" + filename + "' '" + newname + "'")
