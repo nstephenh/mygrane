f323d438bc3a8d7b46da81198ad6cb7a61cc5f56
Noah Stephen Haskell <nstephenh@gmail.com>
2016-09-29 21:51:08 -0400

    nshutil is now no longer a class, just a file with a function in it

--- nshutil.py
+++ nshutil.py
@@ -1,6 +1,5 @@
-class nshutil:
-    def move(src,dest):
-        try:
-            os.rename(src, dest)
-        except OSError:
-            os.system("mv '''" + src + "''' '''" + dest + "'''")
+def move(src,dest):
+    try:
+        os.rename(src, dest)
+    except OSError:
+        os.system("mv '''" + src + "''' '''" + dest + "'''")
