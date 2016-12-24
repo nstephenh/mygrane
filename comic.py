1e5ddd95869ecac99a3ba2e21b41334a21a24444
Noah Stephen Haskell <nstephenh@gmail.com>
2016-12-24 10:35:46 -0500

    Commented out the ident line used for gui which caused errors when sorting nonstandard asci characters

--- comic.py
+++ comic.py
@@ -38,7 +38,7 @@ class Comic:
         self.tags = []
         self.set_info_from_name()
         junk = self.title + str(self.issue) + str(self.pubyear) + self.containing_directory.split('/')[-1]
-        self.ident = hashlib.md5(junk.encode()).hexdigest()
+        #self.ident = hashlib.md5(junk.encode()).hexdigest()
 
     def __str__(self):
         return self.title + " " + str(self.issue) + " (" + str(self.pubyear) + ")"
