a3633baa2ba628900273891fbee376c9399196a2
Noah Haskell <nstephenh@gmail.com>
2018-09-19 18:32:26 -0400

    Attempted cobra like syntax with argparse, last commit before go port

--- Mygrane.py
+++ Mygrane.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 # Main controller for a mygrane instance
 import argparse
+import configparser
 import mygrane
 import mygrane.collection
 #import mygrane.webserve
@@ -11,59 +13,30 @@ Library = None
 
 
 def main():
-        print("Starting instance of Mygrane")
-        parser = argparse.ArgumentParser(description='Comic Library Manager')
-        parser.add_argument("-l", help="Directory where comics are stored")
-        parser.add_argument("--config", help="directory for config files")
+    parser = argparse.ArgumentParser(description='Comic Library Manager')
+    parser.add_argument("--initdb", help="Initialize a database in the default directory")
+    parser.add_argument("--db", help="Alternate database location", type=str)
 
-        # Load information from config file if not specified as arguments
-        args = parser.parse_args()
-        #libLoc = args[0]
-        libLoc = "/home/nsh/Documents/Comics To Read/All of Marvel"
 
-        #_thread.start_new_thread(mygrane.webserve.start_webserve(), ())
+    parser.add_argument("--importdir", help="Import all comics in a specified import directory (recursive)")
+    parser.add_argument("--mv", help="flag move all comics to specified directory", type=str)
+    parser.add_argument("--format", help="Specify format for names")
 
-        # Run a loop of asking for commands and running them.
-        end = False
-        while (not end):
-            try:
-                userinput = input("> ")
-                cmd = userinput.lower().split(" ")[0]
-                if cmd == "help":
-                    print("""Available commands:
-                          quit         -- Quits Mygrane
-                          init <loc>        -- Initializes Library
-                          sort         -- Sorts library
-                          print        -- Prints library contents
-                            """)
-                elif cmd == "init":
-                    try:
-                        path = " ".join(userinput.split(" ")[1:])
-                        if not path is "":
-                            libLoc = path
-                    except Exception:
-                        # Path not provided, assume one is already provided
-                        pass
-                    if libLoc is None:
-                        raise Exception("No Library provided")
-                    print("Opening library at location: " + libLoc)
-                    Library = mygrane.collection.Collection(libLoc)
-                elif cmd in ["print", "ls"]:
-                    if Library is None:
-                        raise Exception("No Library provided")
-                    print(Library)
-                elif cmd in ["sort"]:
-                    if Library is None:
-                        raise Exception("No Library provided")
-                    try:
-                        Library.sort(test=False,allow_duplicates="Delete")
-                    except Exception as detail:
-                        print(detail)
-                elif cmd in ["exit", "quit", "q"]:
-                    print("Goodbye")
-                    quit()
-            except Exception as detail:
-                print(detail)
+
+    args = parser.parse_args()
+
+    #Init variables like db location
+    db = "./database.sqlite"
+    if args.db:
+        db = args.db
+
+    #Run selected command
+    if args.initdb:
+        pass
+    elif args.importdir:
+        pass
+    elif args.list:
+        pass
 
 
 
