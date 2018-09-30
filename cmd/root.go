0e12b1e82f0525ce161fe3bd7b20d12e090315c0
Noah Haskell <nstephenh@gmail.com>
2018-09-30 17:02:24 -0400

    Added viper config

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"os"
 )
 
@@ -13,6 +15,50 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Please specify a command: init, ls, import, help")
 	},
+}
+
+var cfgFile = "$HOME/.mygrane"
+
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mygrane.yaml)")
+	rootCmd.PersistentFlags().Bool("viper", true, "Use Viper for configuration")
+}
+
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	viper.SetConfigType("yaml")
+
+	// Find home directory.
+	home, err := homedir.Dir()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+		viper.SetConfigType("yaml")
+	} else {
+
+		// Search config in home directory
+
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".mygrane")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		//if there's no config create a new one
+		fmt.Println("Can't read config:", err)
+		fmt.Println("Creating new config file")
+		if _, err := os.Create(home + "/.mygrane.yaml"); err != nil {
+			fmt.Println("Can't write default config:", err)
+			os.Exit(1)
+		}
+	}
 }
 
 func Execute() {
