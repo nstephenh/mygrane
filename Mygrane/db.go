c187396f6de1e55e941825360c03ff9011ec872c
Noah Haskell <nstephenh@gmail.com>
2018-10-23 17:11:02 -0400

    Correctly Configured Database

--- Mygrane/db.go
+++ Mygrane/db.go
@@ -16,12 +16,35 @@ func Init_DB(dblocation string) {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		dblocation = home + ".Mygrane.db"
+		dblocation = home + "/" + ".Mygrane.db"
 	}
 	database, _ := sql.Open("sqlite3", dblocation)
-	database.Prepare(`CREATE TABLE IF NOT EXISTS COMICS(
+	/*
+	Database tables:
+	Collections - groups of comics, can be titles, reading orders, etc
+	rel_Collection_Comic - relation table between ^ and v
+	Comics - Stores indivudal comics
+	rel_Comic_File - relation table between ^ and v
+	Files - Stores the individual instances of files, each cbz etc
+	 */
+
+	statement, err := database.Prepare(`CREATE TABLE IF NOT EXISTS COMICS(
 		id INTEGER PRIMARY KEY, title TEXT);
 
 `)
+	fmt.Println(err)
+	statement.Exec()
+	statement, _ = database.Prepare(`CREATE TABLE IF NOT EXISTS COLLECTIONS(
+		id INTEGER PRIMARY KEY, title TEXT);
+`)
+	statement.Exec()
+
+	statement, _ = database.Prepare(`CREATE TABLE IF NOT EXISTS FILES(
+		id INTEGER PRIMARY KEY, path TEXT, hash TEXT);
+`)
+	statement.Exec()
+	fmt.Println("Database Initiated")
+
+
 
 }
