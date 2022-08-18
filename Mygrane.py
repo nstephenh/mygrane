d68cbe9ecacd66ee67327ac2cda1846b6151d018
Noah Stephen Haskell <nstephenh@gmail.com>
2022-08-18 18:21:51 -0500

    Working version python 3

--- Mygrane.py
+++ Mygrane.py
@@ -1,10 +1,10 @@
+#!/usr/bin/python3
+
 # Main controller for a mygrane instance
 import argparse
 
 import mygrane
 import mygrane.collection
-
-Library = None
 
 
 def main():
@@ -14,8 +14,8 @@ def main():
     parser.add_argument("--config", help="directory for config files")
 
     # Load information from config file if not specified as arguments
-    args = parser.parse_args()
-    libLoc = args[0]
+    lib_loc = None
+    library = None
 
     # Run a loop of asking for commands and running them.
     end = False
@@ -24,33 +24,58 @@ def main():
             userinput = input("> ")
             cmd = userinput.lower().split(" ")[0]
             if cmd == "help":
-                print("""Available commands:
-                          quit         -- Quits Mygrane
-                          init <loc>        -- Initializes Library
-                          sort         -- Sorts library
-                          print        -- Prints library contents
-                            """)
+                print("""
+Available commands:
+    quit            Quits Mygrane
+    init <loc>      Initializes Library
+    test            Runs a sort in "Test mode" where it does not move any files
+    sort            Sorts library, not allowing duplicates
+    sortdupes       Sorts library, allowing duplicates
+    sortdelete      Sorts library, deleting duplicates
+    print           Prints library contents
+""")
             elif cmd == "init":
                 try:
                     path = " ".join(userinput.split(" ")[1:])
-                    if not path is "":
-                        libLoc = path
+                    if path != "":
+                        lib_loc = path
                 except Exception:
                     # Path not provided, assume one is already provided
                     pass
-                if libLoc is None:
+                if lib_loc is None:
                     raise Exception("No Library provided")
-                print("Opening library at location: " + libLoc)
-                Library = mygrane.collection.Collection(libLoc)
+                print("Opening library at location: " + lib_loc)
+                library = mygrane.collection.Collection(lib_loc)
             elif cmd in ["print", "ls"]:
-                if Library is None:
+                if library is None:
                     raise Exception("No Library provided")
-                print(Library)
+                print(library)
             elif cmd in ["sort"]:
-                if Library is None:
+                if library is None:
                     raise Exception("No Library provided")
                 try:
-                    Library.sort(test=False, allow_duplicates="Delete")
+                    library.sort(test=False, allow_duplicates="False")
+                except Exception as detail:
+                    print(detail)
+            elif cmd in ["test"]:
+                if library is None:
+                    raise Exception("No Library provided")
+                try:
+                    library.sort(test=True, allow_duplicates="False")
+                except Exception as detail:
+                    print(detail)
+            elif cmd in ["sortdupes"]:
+                if library is None:
+                    raise Exception("No Library provided")
+                try:
+                    library.sort(test=False, allow_duplicates="True")
+                except Exception as detail:
+                    print(detail)
+            elif cmd in ["sortdelete"]:
+                if library is None:
+                    raise Exception("No Library provided")
+                try:
+                    library.sort(test=False, allow_duplicates="Delete")
                 except Exception as detail:
                     print(detail)
         except Exception as detail:
