22af9da9562937bda7516f1b0cff56da0b681ede
Noah Stephen Haskell <nstephenh@gmail.com>
2023-10-16 18:44:11 -0500

    Remove unused GUI settings, fix a typo

--- mygrane/preferences.py
+++ mygrane/preferences.py
@@ -1,11 +1,9 @@
-window_width = 700
-cover_width = 300
 
 input_directory = "/home/nsh/Documents/Comics To Sort"
 library_directory = "/home/nsh/Documents/Comics Library (symlinks)"
 
 use_links = True
-hardlink = True  # Use Hardlinks by default, switch to symlinks of false
+hardlink = True  # Use Hardlinks by default, switch to symlinks if false
 
 verbose = False
 
